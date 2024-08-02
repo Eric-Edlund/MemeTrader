7e0d6d302bf10c494b9d745a8b3bb98d2979ef95
Eric Edlund <ericedlund2017@gmail.com>
2024-08-01 20:40:04 -0400

    Set production articles images url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production' ? 'https://memetrader.net' : 'http://localhost:8001';
 console.log(API_URL)
-export const ARTICLE_IMAGES_URL = process.env.NODE_ENV === 'production' ? 'https:production...' : 'http://localhost:40495'
+export const ARTICLE_IMAGES_URL = process.env.NODE_ENV === 'production' ? 'https://memetrader.net/images/articles' : 'http://localhost:40495'
