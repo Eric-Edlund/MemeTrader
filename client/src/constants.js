e0267d059b6c8d6ecdd0924664723596c210834b
Eric Edlund <ericedlund2017@gmail.com>
2024-08-03 10:56:14 -0400

    Change dev api for client

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
 export const API_URL = process.env.NODE_ENV === 'production' ? 'https://memetrader.net' : 'http://localhost:8001';
-export const ARTICLE_IMAGES_URL = process.env.NODE_ENV === 'production' ? 'https://memetrader.net/images/articles' : 'http://localhost:40495'
+export const ARTICLE_IMAGES_URL = process.env.NODE_ENV === 'production' ? 'https://memetrader.net/images/articles' : 'http://localhost:8080/articles'
